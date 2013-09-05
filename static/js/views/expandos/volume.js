ecae29acf9894fef101cf0ba089c6ca1f07d9b38
dkavanagh <dkavanagh@gmail.com>
2013-09-05 13:03:10 -0400

    EUCA-7463, fixed instance refs in volume expando

--- static/js/views/expandos/volume.js
+++ static/js/views/expandos/volume.js
@@ -11,7 +11,8 @@ define([
       var tmp = this.model ? this.model : new Backbone.Model();
       this.model = new Backbone.Model();
       this.model.set('volume', tmp);
-      this.model.set('snapshot', app.data.snapshots.get(this.model.get('volume').get('snapshot_id')));
+      this.model.set('snapshot', app.data.snapshots.get(tmp.get('snapshot_id')));
+      this.model.set('instance_name', app.data.instances.get(tmp.get('attach_data').instance_id).get('display_id'));
       this.scope = this.model;
       this._do_init();
 
