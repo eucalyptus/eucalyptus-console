4c32705be5887029b825bc8cfd75b214c5a4ad8e
Kamal Gill <kamalgill@mac.com>
2013-09-13 14:03:31 -0700

    Can't use iframe-based transports due to our X-Frame-Options='DENY' setting. EUCA-7380

--- static/js/eucadata.js
+++ static/js/eucadata.js
@@ -138,7 +138,11 @@
       }
       var host_port = url.substring(url.indexOf('://')+3);
       host_port = host_port.substring(0, host_port.indexOf('/'));
-      var push_socket = new SockJS(protocol + '://' + host_port + '/push');
+      var push_socket = new SockJS(protocol + '://' + host_port + '/push', {
+          protocols_whitelist: [
+              'websocket', 'xdr-streaming', 'xhr-streaming', 'xdr-polling', 'xhr-polling', 'jsonp-polling'
+          ]
+      });
       console.log('PUSH>>> established connection');
       push_socket.onmessage = function(evt) {
         var res = $.parseJSON(evt.data);
