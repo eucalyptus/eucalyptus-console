28df94b61fecec136205b85e5c66b5a45f678e3f
dkavanagh <dkavanagh@gmail.com>
2013-09-23 16:03:27 -0400

    added executableby=self to user image results

--- eucaconsole/botoclcinterface.py
+++ eucaconsole/botoclcinterface.py
@@ -89,6 +89,7 @@ class BotoClcInterface(ClcInterface):
 
     def get_users_images(self, owners=None, filters=None, callback=None):
         obj = self.conn.get_all_images(owners='self', executable_by=None, filters=filters)
+        obj.append(self.conn.get_all_images(owners=None, executable_by='self', filters=filters))
         if self.saveclcdata:
             self.__save_json__(obj, "mockdata/Images.json")
         return obj
