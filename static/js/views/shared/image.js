ae7e40b5fb03faa970a3492d1978491cbda1e064
Ean Schuessler <ean@brainfood.com>
2013-10-11 01:15:11 -0500

    EUCA-7828
    A maybe not so bad first cut at using airbnb infinite scroll to handle search selectors in create instance/create launch config.

--- static/js/views/shared/image.js
+++ static/js/views/shared/image.js
@@ -3,10 +3,12 @@ define([
     'backbone',
     'app',
   'text!./image.html!strip',
+  'text!./image-infiniteitem.html!strip',
   'rivets',
   'views/searches/image',
-  './model/blockmap'
-	], function( _, Backbone, app, template, rivets, imageSearch, BlockMap ) {
+  './model/blockmap',
+  'infinity'
+	], function( _, Backbone, app, template, itemTemplate, rivets, imageSearch, BlockMap, infinity ) {
 	return Backbone.View.extend({
             title: app.msg('launch_instance_section_header_image'),
             next: app.msg('launch_instance_btn_next_type'),
@@ -14,6 +16,7 @@ define([
             image_selected: null,
 
             initialize : function(args) {
+              infinity.config.PAGE_TO_SCREEN_RATIO = 5;
               var self = this;
               var imgSource = app.data.allimages;
               if (app.aws.aws_account) {
@@ -103,14 +106,99 @@ define([
             self.render();
           });
 
+
           scope.images = scope.search.filtered;
           this.scope = scope;
-          scope.search.filtered.on('add remove sync change reset', function() {
-              self.render();
-          });
 
          $(this.el).html(template)
          this.rView = rivets.bind(this.$el, this.scope);
+
+          var $itmpl = $('#launch-images', self.el).clone();
+
+          $('#launch-images', self.el).remove();
+          var updateInfinity = _.debounce(function() {
+              console.log('begin infinity init');
+
+              if (self.infinity != undefined) { 
+                  console.log('removed old el', self.el);
+                  $('#launch-images', self.el).remove();
+                  self.infinity.remove(); 
+                  self.infinit = undefined;
+              }
+              
+              var $newtmpl = $itmpl.clone(); 
+              $('#table-wrapper', self.el).prepend($newtmpl);
+
+              self.infinity = new infinity.ListView($newtmpl, {useElementScroll: true});
+              var count = 0;
+
+              var isSelected = function(image) {
+                  if (self.model.get('id') == image.get('id')) {
+                      return ' selected-row';
+                  } 
+                  return '';
+              }
+
+              var setClass = function(image) {
+                    return inferImage(image.get('location'), image.get('description'), image.get('platform'));
+              }
+
+              var added = 0;
+              var totalAdded = 0;
+              var doWork = function() {
+                  // console.log('start work');
+                  var start = new Date().getTime();
+
+                  while (count < scope.search.filtered.length && (new Date().getTime() - start < 500)) {
+                      var image = scope.search.filtered.at(count);
+                      var $tmpl = $(itemTemplate);
+                      rivets.bind($tmpl, new Backbone.Model({
+                            image:image,
+                            formatName: DefaultEncoder().encodeForHTML(image.get('name')),
+             
+                            formatDescription: DefaultEncoder().encodeForHTML(image.get('description')),
+
+                            isOdd: (count % 2) ? 'even' : 'odd',
+
+                            isSelected: isSelected,
+
+                            setClass: setClass
+                      }));
+                      self.infinity.append($tmpl);
+                      count++;
+                      added++;
+                  }
+
+                  // console.log('count: ' + count);
+
+                  if (count < scope.search.filtered.length && added < 100) { 
+                      _.defer(doWork);
+                  } else {
+                      // console.log('end infinity init:', added);
+                      totalAdded += added;
+                      added = 0;
+                  }
+              }
+              doWork();
+
+              var $scrollbox = $($newtmpl.children().get(0));
+              $newtmpl.on('scroll', _.debounce(function() {
+                  // console.log('SCROLL!', $scrollbox.height(), $newtmpl.scrollTop());
+                  if (added < scope.search.filtered.length && 
+                      $scrollbox.height() == $newtmpl.scrollTop() + $newtmpl.height()) {
+                      doWork();
+                  }
+              }));
+
+          }, 2000);
+
+              self.render();
+          scope.search.filtered.on('add remove sync change reset', function() {
+              updateInfinity();
+          });
+
+          updateInfinity();
+
          this.render();
 
          if(this.model.get('image') != undefined) {
