345a11fc0d2ca9fef0edb00145b71a9409e49e5a
dkavanagh <dkavanagh@gmail.com>
2013-10-11 21:28:40 -0400

    EUCA-7858 also wired expando to use proper collection that search uses

--- static/js/views/landing_pages/landing_page_images.js
+++ static/js/views/landing_pages/landing_page_images.js
@@ -30,7 +30,10 @@ define([
                   // IF EXPANDED, APPEND THE RENDER EXPANDED ROW VIEW TO THE PREVIOUS PLACEHOLDER, MATCHED BY ITEM'S ID
                   require(['app', 'views/expandos/image'], function(app, expando) {
                     var $el = $('<div>');
-                    new expando({el: $el, model: app.data.allimages.where({id: thisID})[0] });
+                    new expando({el: $el,
+                                 model: self.scope.get('collection').where({id: thisID})[0],
+                                 collection: self.scope.get('collection')
+                                });
                     $('#expanded-' + thisID).children().remove();
                     $('#expanded-' + thisID).append($el);
                   });
